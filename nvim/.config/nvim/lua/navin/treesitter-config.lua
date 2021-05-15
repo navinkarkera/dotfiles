7acc65ad49c51a103037a88cdb0149ac74a3a336
Navin Karkera <navin@disroot.org>
2021-05-15 14:53:22 +0530

    refactor: list of languages for treesitter-config
    Signed-off by: Navin Karkera <navin@disroot.org>

--- nvim/.config/nvim/lua/navin/treesitter-config.lua
+++ nvim/.config/nvim/lua/navin/treesitter-config.lua
@@ -1,5 +1,17 @@
 require'nvim-treesitter.configs'.setup {
-  ensure_installed = { "python", "html", "javascript", "typescript", "rust", "bash", "json", "lua", "toml", "vue", "css" },     -- one of "all", "language", or a list of languages
+  ensure_installed = {
+      "python",
+      "html",
+      "javascript",
+      "typescript",
+      "rust",
+      "bash",
+      "json",
+      "lua",
+      "toml",
+      "vue",
+      "css",
+  }, -- one of "all", "language", or a list of languages
   highlight = {
     enable = true,              -- false will disable the whole extension
     incremental_selection = { enable = true },
