6be4c21a8287de1994f652d69b3c28e137c3dca0
Navin Karkera <navin@disroot.org>
2021-06-26 18:48:10 +0530

    feat: project-scripts updates

--- nvim-minimal/.config/nvim/lua/treesitter-config.lua
+++ nvim-minimal/.config/nvim/lua/treesitter-config.lua
@@ -14,21 +14,11 @@ require'nvim-treesitter.configs'.setup {
   }, -- one of "all", "language", or a list of languages
   highlight = {
     enable = true,              -- false will disable the whole extension
-    incremental_selection = { enable = true },
     indent = {
       enable = true
     },
     autotag = {
       enable = true
-    },
-    textobjects = {
-        enable = true,
-        keymaps = {
-          ["af"] = "@function.outer",
-          ["if"] = "@function.inner",
-          ["ac"] = "@class.outer",
-          ["ic"] = "@class.inner",
-        }
     },
   },
 }
