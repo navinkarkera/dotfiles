763f97d554b20d75676f544926e81ce9bbaa9ff0
Navin Karkera <navin@disroot.org>
2021-06-26 19:55:54 +0530

    refactor: disable treesitter indent

--- nvim-minimal/.config/nvim/lua/treesitter-config.lua
+++ nvim-minimal/.config/nvim/lua/treesitter-config.lua
@@ -15,7 +15,7 @@ require'nvim-treesitter.configs'.setup {
   highlight = {
     enable = true,              -- false will disable the whole extension
     indent = {
-      enable = true
+      enable = false
     },
     autotag = {
       enable = true
