1d911325f38463ec02e249ca752c8b529bd5165b
Navin Karkera <navin@opencraft.com>
2024-02-16 20:05:36 +0530

    chore: update

--- nvim/.config/nvim/lua/custom/plugins/taskwiki-conf.lua
+++ nvim/.config/nvim/lua/custom/plugins/taskwiki-conf.lua
@@ -2,6 +2,6 @@ return {
   "tools-life/taskwiki",
   init = function()
     vim.g.taskwiki_taskrc_location = "~/.config/task/taskrc"
-    vim.g.taskwiki_data_location = "~/.config/task/"
+    vim.g.taskwiki_data_location = "~/Documents/notes/tasks/"
   end,
 }
