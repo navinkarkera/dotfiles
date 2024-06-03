a134410edaf583e4e87f8ab4396d6c65bf84d4bd
Navin Karkera <navin@opencraft.com>
2024-06-03 20:53:29 +0530

    feat: hurl conf in mdeval
    Signed-off-by: Navin Karkera <navin@opencraft.com>

--- nvim/.config/nvim/lua/custom/plugins/mdeval-conf.lua
+++ nvim/.config/nvim/lua/custom/plugins/mdeval-conf.lua
@@ -3,8 +3,33 @@ return {
   config = function()
     require 'mdeval'.setup({
       require_confirmation=false,
-      allowed_file_types={'bash', 'python', 'lua'},
-      eval_options={},
+      allowed_file_types={'bash', 'python', 'lua', 'hurl', 'hurlv', 'hurlt', 'hurltv'},
+      eval_options={
+        hurl = {
+          command = {"hurl", "--no-color", "--variables-file", "/home/navin/.local/share/vars.env"},
+          language_code = "hurl",
+          exec_type = "interpreted",
+          extension = "hurl",
+        },
+        hurlv = {
+          command = {"hurl", "--no-color", "-v", "--variables-file", "/home/navin/.local/share/vars.env"},
+          language_code = "hurlv",
+          exec_type = "interpreted",
+          extension = "hurl",
+        },
+        hurlt = {
+          command = {"hurl", "--no-color", "--test", "--variables-file", "/home/navin/.local/share/vars.env"},
+          language_code = "hurlt",
+          exec_type = "interpreted",
+          extension = "hurl",
+        },
+        hurltv = {
+          command = {"hurl", "--no-color", "-v", "--test", "--variables-file", "/home/navin/.local/share/vars.env"},
+          language_code = "hurltv",
+          exec_type = "interpreted",
+          extension = "hurl",
+        },
+      },
     })
   end
 }
