4cbdeec482b59091b9fe7bd5de4155b2b4fd91e2
Navin Karkera <navin@opencraft.com>
2024-07-26 20:15:07 +0530

    chore: update

--- nvim/.config/nvim/lua/custom/plugins/treesj-conf.lua
+++ nvim/.config/nvim/lua/custom/plugins/treesj-conf.lua
@@ -2,7 +2,9 @@ return {
   'Wansmer/treesj',
   dependencies = { 'nvim-treesitter/nvim-treesitter' },
   config = function()
-    require('treesj').setup({--[[ your config ]]})
+    require('treesj').setup({
+      use_default_keymaps = false,
+    })
   end,
   keys = {
     { "gS", "<cmd>TSJToggle<CR>", desc = "Split or join" },
