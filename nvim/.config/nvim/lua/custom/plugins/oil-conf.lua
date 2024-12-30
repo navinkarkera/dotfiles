50e310043ff56b51dad5cb2720c46b1280b83dfe
Navin Karkera <navin@opencraft.com>
2024-12-30 21:12:15 +0530

    feat: floating oil

--- nvim/.config/nvim/lua/custom/plugins/oil-conf.lua
+++ nvim/.config/nvim/lua/custom/plugins/oil-conf.lua
@@ -1,8 +1,35 @@
 return {
   "stevearc/oil.nvim",
-  opts = {},
+  opts = {
+    float = {
+      -- Padding around the floating window
+      padding = 2,
+      max_width = 250,
+      max_height = 60,
+      border = "rounded",
+      win_options = {
+        winblend = 0,
+      },
+      -- optionally override the oil buffers window title with custom function: fun(winid: integer): string
+      get_win_title = nil,
+      -- preview_split: Split direction: "auto", "left", "right", "above", "below".
+      preview_split = "auto",
+    },
+    win_options = {
+      wrap = false,
+      signcolumn = "no",
+      cursorcolumn = false,
+      foldcolumn = "0",
+      spell = false,
+      list = false,
+      conceallevel = 3,
+      concealcursor = "nvic",
+      number = false,
+      relativenumber = false,
+    },
+  },
   dependencies = { "nvim-tree/nvim-web-devicons" },
   keys = {
-    { "<leader>to", "<cmd>Oil<CR>", desc = "Open parent directory" },
+    { "<leader>to", "<cmd>lua require('oil').toggle_float()<CR>", desc = "Open parent directory" },
   }
 }
